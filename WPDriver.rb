c087e9ec9ececb535ae6ac1a177f39af4cdd7d47
Prashanth Sams <sams.prashanth@gmail.com>
2015-02-19 18:41:23 +0900

    Update WPDriver.rb

--- WPDriver.rb
+++ WPDriver.rb
@@ -27,10 +27,10 @@ class WPDriver
     puts 'inserted username'
     @driver.find_element(:id, "user_pass").send_keys "nitrous_hacker1"
     puts 'inserted password'
-    @driver.find_element(:id, "wp-submit").click
+    @driver.find_element(:name, "wp-submit").click
     puts 'clicked submit button'
     sleep 5
-    return Commented.new(@driver)
+    return WPDriver.new(@driver)
   end
 
   def skip_login
