7c940c3daac5134d179a8e3b05126a91b5ae24f1
Prashanth Sams <sams.prashanth@gmail.com>
2015-04-02 17:50:45 +0900

    Update spec_helper.rb

--- spec_helper.rb
+++ spec_helper.rb
@@ -1,5 +1,5 @@
 # ENV['HTTP_PROXY'] = ENV['http_proxy'] = nil
-# require File.expand_path("../tests/Google.rb", __FILE__)
+require File.expand_path("../tests/Google.rb", __FILE__)
 
 RSpec.configure do |config|
 
